9b22231177c2d4fc2a9fcc56f228f5483b5456c9
Horace <dbmhorace@gmail.com>
2021-10-23 23:28:28 +0300

    Small modif

--- HackerEarth_Challenges/2_arrays.py
+++ HackerEarth_Challenges/2_arrays.py
@@ -4,6 +4,7 @@
 You are given  arrays  and , each of the size . Each element of these arrays is either a positive integer or . The total number of  that can appear over these  arrays are  and .
 
 Now, you need to find the number of ways in which we can replace each  with a non-negative integer, such that the sum of both of these arrays is equal.
+
 '''
 
 # N = int(input())
